9f1347dbb6ce1b550c80d0ee90d3da5cfec75473
Squiggle9105 <khoyt7168@gmail.com>
2026-05-04 10:54:05 -0700

    limbless Droppy rearranging JS to other files

---
+++ buttons.js
@@ -0,0 +1,33 @@
+//sets ozDrank to 0
+function clearOz(){
+  ozDrank = 0;
+  load();
+}
+
+// will add 4 to ozDrank if button is pressed
+function oz4(){
+  ozDrank = ozDrank + 4;
+  alert(ozDrank);
+  load();
+}
+
+// will add 8 to ozDrank if button is pressed
+function oz8(){
+  ozDrank = ozDrank + 8;
+  alert(ozDrank);
+  load();
+}
+
+// will add 16 to ozDrank if button is pressed
+function oz16(){
+  ozDrank = ozDrank + 16;
+  alert(ozDrank);
+  load();
+}
+
+// will add 32 to ozDrank if button is pressed
+function oz32(){
+  ozDrank = ozDrank + 32;
+  alert(ozDrank);
+  load();
+}
--- chart1.js
+++ chart1.js
@@ -1,17 +1,24 @@
 // today's date (Date.js)
 const todayDate = new Date();
+
 // list of days of the week by name
 const weekDays = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
+
 // array that will store oz drank per day
 var dailyOz = [0,0,0,0,0,0,0];
+
 // will store oz drank in one day
 var ozDrank = 0; 
+
 // today as a String
 var today = weekDays[todayDate.getDay()];
+
 // will hold yesterday as a String
 var yesterday = null;
+
 // will hold the day before yesterday (or ereyesterday) as a String
 var ereyesterday = null;
+
 // if today isn't Sunday, yesterday is one spot backward in the array. If today is Sunday, yesterday is at the end of the array.
 if (todayDate.getDay() > 0)
 {
@@ -21,6 +28,7 @@ else
 {
     yesterday = 'Saturday';
 }
+
 // if today is at least Tuesday, the day before yesterday is two spots backward in the array. If today is Monday, it is at the end of the array, and if today is Sunday, it is two spots backward.
 if (todayDate.getDay() > 1)
 {
@@ -34,6 +42,7 @@ else
 {
     ereyesterday = 'Friday';
 }
+
 // fill in values of the graph
 const xValues = [ereyesterday, yesterday, today];
 const yValues = [64,72,ozDrank];
@@ -66,39 +75,11 @@ new Chart (ctx, {
     }
   }
 });
+
 //reloads chart
 function load()
 {
   Chart.resetChart();
 }
-//sets ozDrank to 0
-function clearOz(){
-  ozDrank = 0;
-  load();
-}
 
-// will add 4 to ozDrank if button is pressed
-function oz4(){
-  ozDrank = ozDrank + 4;
-  alert(ozDrank);
-  load();
-}
-// will add 8 to ozDrank if button is pressed
-function oz8(){
-  ozDrank = ozDrank + 8;
-  alert(ozDrank);
-  load();
-}
-// will add 16 to ozDrank if button is pressed
-function oz16(){
-  ozDrank = ozDrank + 16;
-  alert(ozDrank);
-  load();
-}
-// will add 32 to ozDrank if button is pressed
-function oz32(){
-  ozDrank = ozDrank + 32;
-  alert(ozDrank);
-  load();
-}
 
